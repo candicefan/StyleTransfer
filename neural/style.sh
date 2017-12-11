5967eaa569c5c0bc2d47b5f4b074d5c18fedd1a9
Siqi Chen <Siqi Chen>
2017-12-11 02:00:36 -0500

    d

--- neural/style.sh
+++ neural/style.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-5-0.jpg --row 2 --column 3 --selections 2 --selection-weight 5
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-5-0.jpg --row 2 --column 3 --selections 2 --selection-weight 5 --iterations 1000

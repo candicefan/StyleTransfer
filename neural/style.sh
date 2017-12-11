526ee58b767fc085421be406b4277dd28c8bb669
Siqi Chen <Siqi Chen>
2017-12-11 02:13:23 -0500

    d

--- neural/style.sh
+++ neural/style.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-5-0.jpg --row 2 --column 3 --selections 2 --selection-weight 5 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-4-0.jpg --row 2 --column 3 --selections 2 --selection-weight 4 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-3-0.jpg --row 2 --column 3 --selections 2 --selection-weight 3 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-2-0.jpg --row 2 --column 3 --selections 2 --selection-weight 2 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-1-0.jpg --row 2 --column 3 --selections 2 --selection-weight 1 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-8.jpg --row 2 --column 3 --selections 2 --selection-weight 0.8 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-6.jpg --row 2 --column 3 --selections 2 --selection-weight 0.6 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-4.jpg --row 2 --column 3 --selections 2 --selection-weight 0.4 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-2.jpg --row 2 --column 3 --selections 2 --selection-weight 0.2 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-0.jpg --row 2 --column 3 --selections 2 --selection-weight 0 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0.jpg --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-4-0.jpg --row 2 --column 3 --selections 2 --selection-weight 4 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-3-0.jpg --row 2 --column 3 --selections 2 --selection-weight 3 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-2-0.jpg --row 2 --column 3 --selections 2 --selection-weight 2 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-1-0.jpg --row 2 --column 3 --selections 2 --selection-weight 1 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-8.jpg --row 2 --column 3 --selections 2 --selection-weight 0.8 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-6.jpg --row 2 --column 3 --selections 2 --selection-weight 0.6 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-4.jpg --row 2 --column 3 --selections 2 --selection-weight 0.4 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-2.jpg --row 2 --column 3 --selections 2 --selection-weight 0.2 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0-0.jpg --row 2 --column 3 --selections 2 --selection-weight 0 --iterations 1000
+python neural_style_lum.py --content content/2.jpg --styles style/01.jpg --output results/2-1-0.jpg --iterations 1000
